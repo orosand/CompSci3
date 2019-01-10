e1e5c9f6c8e38001dcc53cc096202da44a29392c
ollie-rosand <orosand@putneyschool.org>
2019-01-10 12:04:12 -0500

    Update MergeSort.py

--- MergeSort.py
+++ MergeSort.py
@@ -1,11 +1,28 @@
 
 def mergeSort(list):
-	if (0<(len(list)-1)):
-		m=(len(list)-2)/2
-		list1 = list[:len(list)//2]
-		list2 = list[len(list)//2:]
-	mergeSort(list1)
-	mergeSort(list2)
+	if (len(list)==1):
+		return list
+	list1 = list[:len(list)//2]
+	list2 = list[len(list)//2:]
+	return merge(mergeSort(list1),mergeSort(list2))
+def merge(list1,list2):
+	indexOne = 0
+	indexTwo = 0
+	tempArray = []
+	while indexOne<len(list1) and indexTwo<len(list2):
+		if list1[indexOne]>list2[indexTwo]:
+			tempArray.append(list1[indexOne])
+			indexOne += 1
+		else:
+			tempArray.append(list2[indexTwo])
+			indexTwo += 1
+	while indexOne<len(list1):
+		tempArray.append(list1[indexOne])
+		indexOne += 1
+	while indexTwo<len(list2):
+		tempArray.append(list2[indexTwo])
+		indexTwo += 1
 
 
-mergeSort([1,2,3,4,5,6])
+	return tempArray
+print mergeSort([1,2,45,1,2,5,5134314,314,1,23,436,])
